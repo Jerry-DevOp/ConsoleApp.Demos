9d4bc05f4344c62f13369cc94b932ee50348cd59
Jerry-DevOp <jerrynkutshweu@gmail.com>
2024-06-25 09:08:11 +0200

    Changed the code, to add, firstname and lastname.so we can prompt the user for those details

--- ConsoleAppUserInputDemo/Program.cs
+++ ConsoleAppUserInputDemo/Program.cs
@@ -1,20 +1,18 @@
 ﻿//Declare variable
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
 //prompt user for input
-Console.WriteLine("Please enter your name");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your Firstname");
+firstName = Console.ReadLine();
+Console.WriteLine("Please enter your Lastname");
+lastName = Console.ReadLine();
 Console.WriteLine("Please enter your Age");
 age = Convert.ToInt32(Console.ReadLine());
 
 //Process data
 int workingYearsRemaining = retirementAge - age;
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"working years remaining: {workingYearsRemaining}");
-
-
-
-
-
