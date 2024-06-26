23b2374ac31427410dde2e962d793b11740d84c3
Jerry-DevOp <jerrynkutshweu@gmail.com>
2024-06-26 08:31:29 +0200

    startting with while loops

--- ConsoleAppLoops/Program.cs
+++ ConsoleAppLoops/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿//Print a program that prints
+//Hello world ten times
+
+Console.WriteLine("How many time should we print 'Helllo ,World'");
+int counter = Convert.ToInt32(Console.ReadLine());  
+for(int i = 0; i < counter; i++)
+{
+    Console.WriteLine($"Hello world ?");
+}
