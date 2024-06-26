c4547aa480fffb2ecba525b774caa380a7fefe36
Jerry-DevOp <jerrynkutshweu@gmail.com>
2024-06-26 14:27:24 +0200

    Started learning about exceptions

---
+++ ConsoleAppExceptingHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Write a user program that takes age  
+ * an input and prints it to the screen
+ * Display an error message if an invalid
+    inpput received
+*/
+
+try 
+{
+    Console.Write("How old are you? :");
+    int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Hi you are, {age} old");
+}
+catch(Exception)
+{
+    Console.WriteLine("You have entered an invalid value try again");
+    //throw;
+    // will show the error on the console
+    //
+}
+finally// this block executes no matter what
+{
+    Console.WriteLine("Thank you for using the program");
+}
